ca0b643a6e8a341a058e9fb97d21f0692e4781c2
Tommy Pham <pham.tommy7@gmail.com>
2022-08-21 20:26:54 -0700

    display array and allow random array size

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
 
-let sumbitButton = document.querySelector('#submit');
 let selectionSortButton = document.querySelector('#selectionsort')
 let inputs = document.querySelector('#inputs')
 let input = 10;
 
-sumbitButton.addEventListener('click', createArray(input));
-//selectionSortButton.addEventListener('click', selectionSort(records))
 
 
 function setSize(input) {
-  createArray(input)
+  createArray(input);
 }
 
 function createArray(size) {
-  arraySize = Array.from({length: size}, () => Math.floor(Math.random() * 40));
+  array = Array.from({length: size}, () => Math.floor(Math.random() * 40));
+  let array_space = document.querySelector(".array")
+  let array_elements = array_space.querySelector("div")
+
+  array_space.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  
+  for (let i = 0; i < size; i++) {
+    let element = document.createElement("div");
+    element.classList.add('individual-ints')
+    array_space.insertAdjacentElement("beforeend", element);
+    element.insertAdjacentText("beforeend", array[i])
+  }
 
 }
 
+createArray(input)
 // swap elements in array 
 function swap(arr, index1, index2) {
   let temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
 }
-/*
-function selectionSort(input) {
-  let smallest = 0;
-  for(let i = 0; i < input.length; i++) 
-  {
+selectionSortButton.addEventListener('click', selectionSort)
 
-    for(let j = 0; j < input.length; j++) 
+function selectionSort(input) {
+  for(let i = 0; i < input.length - 1; i++) 
+  {
+    let smallest = i;
+    for(let j = i + 1; j < input.length; j++) 
     {
 
-      if (input[j] < input[i])
+      if (input[j] < input[smallest])
         smallest = j;
-
-      swap(input, smallest, i);
     }
+    swap(input, smallest, i);
 
   }
-  */
+  console.log(input)
+  console.log("test")
+  // document.querySelector("#sorted-array").innerHTML = array
+
 } 
+
+selectionSort(array)
