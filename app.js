ddd809a99fe7dad5916670f51ee3d88b2ee82ea2
txpham209 <89564407+txpham209@users.noreply.github.com>
2022-09-08 21:11:17 -0700

    added bubble sort and merge sort

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-
 let selectionSortButton = document.querySelector('#selectionsort');
 let insertionSortButton = document.querySelector('#insertionsort');
 let mergeSortButton = document.querySelector('#mergesort');
-
+let bubbleSortButton = document.querySelector('#bubblesort')
 let inputs = document.querySelector('#inputs');
 let input = 10;
 
 selectionSortButton.addEventListener('click', selectionSort);
 insertionSortButton.addEventListener('click', insertionSort);
 mergeSortButton.addEventListener('click', mergeSort);
+bubbleSortButton.addEventListener('click', bubbleSort)
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function setSize(input) {
   createArray(input);
@@ -47,11 +51,7 @@ async function selectionSort(delay = 1000) {
       cells[j].style.backgroundColor = "red";
         
       // To pause the execution of code for 300 milliseconds
-      await new Promise((resolve) =>
-        setTimeout(() => {
-          resolve();
-        }, 1000)
-      );
+      await sleep(1000)
        // To store the integer value of jth bar to var1 
       var val1 = parseInt(cells[j].innerHTML);
        // To store the integer value of smallest bar to var2 
@@ -69,19 +69,12 @@ async function selectionSort(delay = 1000) {
         cells[j].style.backgroundColor = "  rgb(24, 190, 255)";
       }
     }
-    var temp1 = cells[smallest].style.height;
     var temp2 = cells[smallest].innerText;
-    cells[smallest].style.height = cells[i].style.height;
-    cells[i].style.height = temp1;
     cells[smallest].innerText = cells[i].innerText;
     cells[i].innerText = temp2;
 
     // To pause the execution of code for 300 milliseconds
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve();
-      }, 1000)
-    );
+    await sleep(1000)
   
     // Provide skyblue color to the (min-idx)th bar
     cells[smallest].style.backgroundColor = "  rgb(24, 190, 255)";
@@ -104,33 +97,21 @@ async function insertionSort(delay = 1000) {
       cells[j].style.backgroundColor = "yellow";
       cells[i].style.backgroundColor = "red";
 
-      await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve();
-      }, 1000)
-    );
+      await sleep(1000)
      // cells[key].style.backgroundColor = "red"
       // To pause the execution of code for 1000 milliseconds
-      console.log(" test " + cells[j].innerText + " > " + " " + key)
       cells[j + 1].innerText = cells[j].innerText;
       j = j - 1;  
-
-      // Provide lightgreen color to the sorted part
 
     }
       cells[j + 1].innerHTML = key;
 
-      // To pause the execution of code for 300 milliseconds
 
       for(var k=i;k>=0;k--){
         cells[k].style.backgroundColor = " rgb(49, 226, 13)";
       // To pause the execution of code for 300 milliseconds
       }
-      await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve();
-      }, 1000)
-    );
+      await sleep(1000)
     }  
 }  
 
@@ -142,39 +123,38 @@ async function mergeSort(delay = 1000) {
   }
 
   mergeSortVisual(arr, 0, arr.length - 1)
-  console.log(arr)
 }
 
 
-function mergeSortVisual(arr, l, r) {
-  let cells = document.querySelectorAll(".individual-ints");
+async function mergeSortVisual(arr, l, r) {
 
   if(l>=r){
     return;//returns recursively
   }
-  console.log("test: " + l + " " + r)
   var m =l+ parseInt((r-l)/2);
-  mergeSortVisual(arr,l,m);
-  cells[l].style.backgroundColor = "yellow";
-  mergeSortVisual(arr,m+1,r);
-  merge(arr,l,m,r);
+  await mergeSortVisual(arr,l,m);
+  await mergeSortVisual(arr,m+1,r);
+  await merge(arr,l,m,r);
+  
+  await sleep(800)
 }
 
-function merge(arr, l, m, r)
+async function merge(arr, l, m, r)
 {
+    console.log(arr)
+    let cells = document.querySelectorAll(".individual-ints");
     var n1 = m - l + 1;
     var n2 = r - m;
   
     // Create temp arrays
     var L = new Array(n1); 
     var R = new Array(n2);
-  
     // Copy data to temp arrays L[] and R[]
     for (var i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (var j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
-  
+
     // Merge the temp arrays back into arr[l..r]
   
     // Initial index of first subarray
@@ -185,7 +165,6 @@ function merge(arr, l, m, r)
   
     // Initial index of merged subarray
     var k = l;
-  
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -195,13 +174,16 @@ function merge(arr, l, m, r)
             arr[k] = R[j];
             j++;
         }
+        cells[k].innerHTML = arr[k];
+        cells[k].style.backgroundColor = "orange"
         k++;
     }
-  
     // Copy the remaining elements of
     // L[], if there are any
     while (i < n1) {
         arr[k] = L[i];
+        cells[k].innerHTML = arr[k];
+        cells[k].style.backgroundColor = "orange"
         i++;
         k++;
     }
@@ -210,14 +192,37 @@ function merge(arr, l, m, r)
     // R[], if there are any
     while (j < n2) {
         arr[k] = R[j];
+        cells[k].innerHTML = arr[k];
+        cells[k].style.backgroundColor = "orange"
         j++;
         k++;
     }
 }
 
+async function bubbleSort() {
+  let cells = document.querySelectorAll(".individual-ints");
+  var i, j;
+  let smallest;
+  for (i = 0; i < cells.length; i++)
+  {
+      for (j = 0; j < cells.length - i - 1; j++) {
+        // To store the integer value of jth bar to var1 
+        var val1 = parseInt(cells[j].innerHTML);
+        // To store the integer value of smallest bar to var2 
+        var val2 = parseInt(cells[j+1].innerHTML);
 
+        cells[j+1].style.backgroundColor = 'green'
+        cells[j].style.backgroundColor = 'blue'
+        if (val1 > val2) {
+          let temp1 = cells[j + 1].innerText
+          cells[j + 1].innerText = cells[j].innerText;
+          cells[j].innerText = temp1;
+        }
+  
+      // To pause the execution of code for 300 milliseconds
+      await sleep(1000)
+      }
+    }
+    cells[0].style.backgroundColor = 'green'
 
-// 6 14 5 9 7 26 8 24 18 14
-// 6 b(14) 5 9 7 26 8 24 18 14
-// key = 1, j = 0
-// 
+}
